96a7d587f319e76b3655217fcc94a8788515ea0d
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2022-04-10 19:10:58 +0530

    Generation of unique id using UUID package and storing state working

--- src/Pages/Homepage/home.page.jsx
+++ src/Pages/Homepage/home.page.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
-import './homepage.styles.css'
-import logo from '../../Assets/code-sync.png'
+import React, { useState } from "react";
+import "./homepage.styles.css";
+import logo from "../../Assets/code-sync.png";
+import { v4 as uuid } from "uuid";
 export const Home = () => {
+
+  const [roomId , setRoomId] = useState('');
+  const [userName , setuserName] = useState('');
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuid();
+    setRoomId(id);
+    // console.log(id);
+  };
+
+
   return (
-    <div className='homePageContainer'>
+    <div className="homePageContainer">
       <div className="formContainer">
-        <img src={logo} className='homepagelogo' alt="code" />
-        <h4 className='main-label'> Paste invitation ROOM ID</h4>
+        <img src={logo} className="homepagelogo" alt="code" />
+        <h4 className="main-label"> Paste invitation ROOM ID</h4>
         <div className="inputContainer">
-          <input type="text" className='inputBox' placeholder='ROOM ID'/>
-          <input type="text" className='inputBox' placeholder='USERNAME'/>
-          <button className='btn joinbtn'>JOIN</button>
-          <span className='createInfo'>
+          <input type="text" className="inputBox" placeholder="ROOM ID" onChange={(e)=> setRoomId(e.target.value)} value={roomId} />
+          <input type="text" className="inputBox" placeholder="USERNAME" onChange={(e)=> setuserName(e.target.value)} value={userName}/>
+          <button className="btn joinbtn">JOIN</button>
+          <span className="createInfo">
             If you don't have an invite then create &nbsp;
-            <a href="google.com" className='createNewBtn'>new room </a>
+            <a
+              onClick={createNewRoom}
+              href="google.com"
+              className="createNewBtn"
+            >
+              new room{" "}
+            </a>
           </span>
         </div>
       </div>
       <footer>
-        <h4>Built with love By <a href="https://github.com/devanshuruhela">Devanshu</a> </h4>
+        <h4>
+          Built with love By{" "}
+          <a href="https://github.com/devanshuruhela">Devanshu</a>{" "}
+        </h4>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
