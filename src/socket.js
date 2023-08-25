001a882399db243a884eb2ae8df7c687d7bf9cda
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2023-08-26 00:22:12 +0530

    socket options config

--- src/socket.js
+++ src/socket.js
@@ -3,9 +3,11 @@ const { io } = require("socket.io-client");
 export const initSocket = async () => {
   const options = {
     "force new connection": true,
+    forceNew:true,
     reconnectionAttempt: "Infinity",
     timeout: 10000,
-    transports: ["websocket"],
+    secure:true,
+    transports: ["websocket" ,  "polling"],
   };
   return io(process.env.REACT_APP_BACKEND_URL, options);
 };
